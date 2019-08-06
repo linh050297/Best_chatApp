98ee134df23178efdae95c463b2f10d24ab0ebb0
Admin <linh050297@gmail.com>
2019-08-06 15:20:09 +0700

    update

---
+++ sh/env.sh
@@ -0,0 +1,9 @@
+
+export DB_CONNECTION=mongodb
+export DB_HOST=localhost
+export DB_PORT=27017
+export DB_NAME=chat_app
+export DB_USERNAME=""
+export DB_PASSWORD=""
+export HOST_NAME=localhost
+export PORT=8017
