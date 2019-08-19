d1084c190e4ab73dd3ed29dff871b0ff2cdadf7e
Admin <linh050297@gmail.com>
2019-08-20 06:21:04 +0700

    notification

--- src/public/js/calculateNotification.js
+++ src/public/js/calculateNotification.js
@@ -13,7 +13,7 @@ function decreaseNumberNotificationContact(className){
     let currentValue = +$(`.${className}`).text(); //dấu cộng chuyển string thành số còn mặc định sẽ là 0 
     currentValue -= 1;
 
-    if(currentValue <= 0 ){
+    if(currentValue === 0 ){
         $(`.${className}`).css("display","none").html("");
     }else{
         $(`.${className}`).css("display","inline").html(currentValue);
