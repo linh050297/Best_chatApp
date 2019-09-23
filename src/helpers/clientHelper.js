4c0574392a0aff445777af2f7dc46e6912b27c98
Admin <linh050297@gmail.com>
2019-09-23 15:41:32 +0700

    config leftsidebar

--- src/helpers/clientHelper.js
+++ src/helpers/clientHelper.js
@@ -1,3 +1,21 @@
+import moment from 'moment';
+
 export let bufferToBase64 = (buffer)=>{
     return Buffer.from(buffer).toString('base64')
-}
+};
+
+export let lastItemOfArray = (array)=>{
+    if(!array.length){
+        return [];
+    }else{
+        return array[array.length - 1];
+    }
+};
+
+export let convertTimestampToHumanTime = (timestamp)=>{
+    if(!timestamp){
+        return '';
+    }else{
+        return moment(timestamp).locale('vi').startOf('seconds').fromNow();
+    }
+};
